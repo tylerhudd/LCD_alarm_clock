513b8a39c478020b5a83ce07c81f0dbdbb7b65ea
Tyler <tylerhudd@gmail.com>
2017-05-05 22:13:00 -0700

    Started timer interrupt sequence
    Added clock incrementing feature and fixed all bugs with LCD and button
    interfacing

--- LCD_alarm_clock/main.c
+++ LCD_alarm_clock/main.c
@@ -8,11 +8,12 @@
  *					stopwatch and display in LCD.
  */ 
 
-#define F_CPU 1000000UL
+#define F_CPU 8000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
 
 // LCD pins
 #define rs PD0
@@ -21,9 +22,9 @@
 #define data PORTB
 
 // push button pins
-#define enter PD0		// ENTER button
-#define inc PD1			// INCREMENT button
-#define dec PD2			// DECREMENT button
+#define enter PC0		// ENTER button
+#define inc PC1			// INCREMENT button
+#define dec PC2			// DECREMENT button
 
 // function declarations
 void lcd_init();						// clears LCD screen and sets position
@@ -36,9 +37,11 @@ void incHr(int CorA);					// increment clock or alarm hours
 void decHr(int CorA);					// decrement clock or alarm hours
 void incMin(int CorAorT, int set);		// increment clock or alarm mins
 void decMin(int CorAorT);				// decrement clock or alarm mins
+void incSec();							// increment clock seconds
 void toggleAMPM(int CorA);				// toggle am/pm
 int setTime(int CorA, int setState);	// set clock or alarm time
 void printTimer();						// print the timer time
+void stopwatch();
 
 // global variables
 int state;
@@ -53,6 +56,13 @@ int alarmAmpm;
 int timerMin;
 int timerSec;
 int runTimer;
+int swSec;
+int SWrun;
+
+ISR(TIMER1_COMPA_vect)
+{
+	incSec();
+}
 
 
 int main()
@@ -67,19 +77,29 @@ int main()
 	min = 0;
 	sec = 0;
 	ampm = 0;
-	alarmHr = 12;
-	alarmMin = 0;
+	alarmHr = 11;
+	alarmMin = 59;
 	alarmSec = 0;
 	alarmAmpm = 0;
 	timerMin = 0;
 	timerSec = 0;
 	runTimer = 0;
+	swSec = 0;
+	SWrun = 0;
 	
+	sei();
 	
 	DDRB=0XFF;			// PORT B: data register for LCD set to output
-	DDRD=0XF8;			// PORT D: PD0:2 push button input, PD3:5 LCD commands output
+	DDRC=0x00;			// PORT C: push button inputs
+	DDRD=0XFF;			// PORT D: PD0:2 LCD commands output
 
 	lcd_init();
+	displayMenuOption(stateSelect);
+	
+	
+	TCCR1B = 0x0C;	// compare value in OCR1A and 1024 prescaler
+	TIMSK1 = 0x02;	// timer mask
+	OCR1A = 36000;
 	
 	while(1)
 	{
@@ -93,89 +113,139 @@ int main()
 		*/
 		
 		
+		if(alarmHr == hr && alarmMin == min && alarmAmpm == ampm) state = 5;
+		
 		switch(state)
 		{
 			case 0 :	// display clock and alarm, menu select
-				if((PIND & 0x01) == 0x01)	// if enter button pressed
+				if((PINC & 0x01) == 0x01)	// if enter button pressed
 				{
 					state = stateSelect;
 					switch(state)
 					{
 						case 1 :  // initial procedure for clock set
 							lcd_cmd(0x01);	//clear screen
-							lcd_str("Set hours");
+							lcd_str("Clock: set hour");
 							break;
 						case 2 : // initial procedure for alarm set
-							
+							lcd_cmd(0x01);	//clear screen
+							lcd_str("Alarm: set hour");
 							break;
 						case 3 : // initial procedure for stopwatch
-							
+							lcd_cmd(0x01);
+							lcd_str("Stopwatch");
+							lcd_cmd(0xC0);
+							SWrun=1;
 							break;
 						case 4:	// initial procedure for timer
 							
 							break;
 					}
 				}
-				if((PIND & 0x02) == 0x02)	// if increment button pressed
+				else if((PINC & 0x02) == 0x02)	// if increment button pressed
 				{
 					if(stateSelect == 4) stateSelect = 0;
 					else stateSelect++;
 					displayMenuOption(stateSelect);
 				}
-				if((PIND & 0x04) == 0x04)	// if decrement button pressed
+				else if((PINC & 0x04) == 0x04)	// if decrement button pressed
 				{
 					if(stateSelect == 0) stateSelect = 4;
 					else stateSelect--;
 					displayMenuOption(stateSelect);
 				}
+				else
+				{
+					lcd_cmd(0x80);
+					printClock(0);
+				}
 				break;
+			
+			
 			
 			case 1 :	// set clock
 				clockSetState = setTime(0, clockSetState);
+				if (clockSetState == 3)
+				{
+					clockSetState = 0;
+					state = 0;
+					stateSelect = 0;
+					lcd_cmd(0x01);
+					printClock(0);
+					displayMenuOption(stateSelect);
+				}
 				break;
+			
+			
+			
 			
 			case 2:	// set alarm
 				alarmSetState = setTime(1, alarmSetState);
+					if (alarmSetState == 3)
+					{
+						alarmSetState = 0;
+						state = 0;
+						stateSelect = 0;
+						lcd_cmd(0x01);
+						printClock(0);
+						displayMenuOption(stateSelect);
+					}
 				break;
+				
 				
 			case 3:	// stopwatch
-				
+				stopwatch();
+				if((PINC & 0x01) == 0x01 && timerMin > 0)	// if enter button pressed
+				{
+					state = 0;
+					stateSelect = 0;
+					lcd_cmd(0x01);
+					printClock(0);
+					displayMenuOption(stateSelect);
+					swSec = 0;
+					SWrun = 0;
+				}
 				break;
 				
+				
 			case 4:	// timer
-				if((PIND & 0x01) == 0x01 && timerMin > 0)	// if enter button pressed
+				if((PINC & 0x01) == 0x01 && timerMin > 0)	// if enter button pressed
 				{
 					runTimer = 1;
 				}
 				if(!runTimer)
 				{
-					if((PIND & 0x02) == 0x02)	// if increment button pressed
+					if((PINC & 0x02) == 0x02)	// if increment button pressed
 					{
 						incMin(2, 0);
 					}
-					if((PIND & 0x04) == 0x04)	// if decrement button pressed
+					if((PINC & 0x04) == 0x04)	// if decrement button pressed
 					{
 						decMin(2);
 					}
 				}
-				printTimer();
+				//printTimer();
 				break;
 			
 			case 5:	// sound the alarm
-				for(int i=0; i<10; i++)
+				for(int i=0; i<3; i++)
 				{
 					lcd_cmd(0x01);
-					_delay_ms(500);
+					_delay_ms(200);
 					lcd_str("!!!!!!Alarm!!!!!!");
 					lcd_cmd(0xC0);
 					lcd_str("*!*!*!*!*!*!*!*!");
-					_delay_ms(500);
+					_delay_ms(200);
+					alarmHr = 11;
+					alarmMin = 59;
 				}
+				state = 0;
+				lcd_cmd(0x01);
+				displayMenuOption(stateSelect);
 				break;
+				
+			default: state = 0;
 			
-			if(alarmHr == hr && alarmMin == min && alarmAmpm == ampm) state = 5;
-			
-			_delay_ms(5);
 		}
 	}
 }
@@ -226,7 +296,7 @@ void displayMenuOption(int stateSel)
 	switch(stateSel)
 	{
 		case 0 :
-			lcd_str("MCU Clock - GG & TH");
+			lcd_str("MCU Clock-GG&TH");
 		break;
 		case 1 :
 			lcd_str("-> Clock Set");
@@ -252,22 +322,32 @@ void printClock(int CorA)
 	switch(CorA)
 	{
 		case 0:	// for clock
-			sprintf(hrChar, "%i", hr);
-			sprintf(minChar, "%i", min);
+			if(hr<10) sprintf(hrChar, "0%i", hr);
+			else sprintf(hrChar, "%i", hr);
+			
+			if(min<10) sprintf(minChar, "0%i", min);
+			else sprintf(minChar, "%i", min);
+			
 			if (ampm == 1) ampmChar = 'P';
 			break;
 		case 1: // for alarm
-			sprintf(hrChar, "%i", alarmHr);
-			sprintf(minChar, "%i", alarmMin);
+			if(alarmHr<10) sprintf(hrChar, "0%i", alarmHr);
+			else sprintf(hrChar, "%i", alarmHr);
+			
+			if(alarmMin<10) sprintf(minChar, "0%i", alarmMin);
+			else sprintf(minChar, "%i", alarmMin);
+			
 			if (alarmAmpm == 1) ampmChar = 'P';
 			break;
 	}
 	
-	lcd_str(hrChar);
+	lcd_data(hrChar[0]);
+	lcd_data(hrChar[1]);
 	lcd_str(":");
 	lcd_str(minChar);
 	lcd_data(ampmChar);
 	lcd_data('M');
+	// for(int i=0; i<7; i++) lcd_str(" ");
 }
 
 void incHr(int CorA)
@@ -305,9 +385,12 @@ void incMin(int CorAorT, int set)
 	switch(CorAorT)
 	{
 		case 0: // for clock
-			if(min == 59) min = 0;
+			if(min == 59)
+			{
+				min = 0;
+				if(set == 0) incHr(0);
+			}
 			else min++;
-			if(set == 0) hr++;
 			break;
 		case 1:	// for alarm
 			if(alarmMin == 59) alarmMin = 0;
@@ -337,6 +420,17 @@ void decMin(int CorAorT)
 	}
 }
 
+void incSec()
+{
+	if(sec==59)
+	{
+		sec = 0;
+		incMin(0,0);
+	}
+	else sec++;
+	if(SWrun) swSec++;
+}
+
 void toggleAMPM(int CorA)
 {
 	switch(CorA)
@@ -356,7 +450,7 @@ int setTime(int CorA, int setState)
 {
 	lcd_cmd(0xC0);	// set position [2,1]
 	printClock(CorA);
-	if((PIND & 0x02) == 0x02)	// if increment button pressed
+	if((PINC & 0x02) == 0x02)	// if increment button pressed
 	{
 		switch(setState)
 		{
@@ -371,7 +465,7 @@ int setTime(int CorA, int setState)
 				break;
 		}
 	}
-	if((PIND & 0x04) == 0x04)	// if decrement button pressed
+	if((PINC & 0x04) == 0x04)	// if decrement button pressed
 	{
 		switch(setState)
 		{
@@ -386,27 +480,50 @@ int setTime(int CorA, int setState)
 				break;
 		}
 	}
-	if((PIND & 0x01) == 0x01)	// if enter button pressed
+	if((PINC & 0x01) == 0x01)	// if enter button pressed
 	{
 		switch(setState)
 		{
 			case 0:
 				lcd_cmd(0x01);
 				lcd_cmd(0x80);
-				lcd_str("Set minutes");
+				if(!CorA) lcd_str("Clock: ");
+				else lcd_str("Alarm: ");
+				lcd_str("set mins");
 				setState ++;
 				break;
 			case 1:
 				lcd_cmd(0x01);
 				lcd_cmd(0x80);
-				lcd_str("Set AM/PM");
+				if(!CorA) lcd_str("Clock: ");
+				else lcd_str("Alarm: ");
+				lcd_str("set AM/PM");
 				setState ++;
 				break;
 			case 2:
+				setState ++;
+				break;
+			default:
 				state = 0;
 				setState = 0;
 				break;
 		}
 	}
 	return setState;
+}
+
+void stopwatch(int swsec)
+{
+	char mins = '0';
+	char secs[2];
+	
+	sprintf(mins, "%i", swsec/60);
+	if(swsec%60<10) sprintf(secs, "0%i", swsec%60);
+	else sprintf(secs, "%i", swsec%60);
+	
+	lcd_cmd(0xc0);
+	lcd_data(mins);
+	lcd_str(":");
+	lcd_data(secs[0]);
+	lcd_data(secs[1]);
 }
